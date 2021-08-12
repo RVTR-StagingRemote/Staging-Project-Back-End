a9cc15d8cd7d85cccbe7a75558daa202e927d719
daustindev <daustindev@gmail.com>
2021-08-12 13:26:47 -0500

    Added initial models

---
+++ REST/Models/CoursesTopicsJoin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Models
+
+public class CoursesTopicsJoin
+{
+	public CoursesTopicsJoin()
+	{
+		public int JoinId { get; set; }
+		public int CourseId { get; set; }
+		public int TopicId { get; set; }
+	}
+}
+
+
---
+++ REST/Models/Topics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models
+
+public class Topics
+{
+	public Topics()
+	{
+		public int TopicId { get; set; }
+		public string TopicName { get; set; }
+	}
+}
