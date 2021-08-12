bdfa292cec343d6fe5ddda4cf07e85da3a98023c
Mohcka <mikeduels@yahoo.com>
2021-08-12 15:58:28 -0500

    Merge remote-tracking branch 'origin/main' into efcore-setup

--- REST/Startup.cs
+++ REST/Startup.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using REST.DataLayer;
+using Microsoft.OpenApi.Models;
 
 namespace REST
 {
@@ -32,6 +34,10 @@ namespace REST
             services.AddDbContext<BatchesDBContext>(opts => opts.UseNpgsql(Configuration.GetConnectionString("batchesDB")));
 
             services.AddSingleton(_ => Configuration);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Client_Portal", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +46,8 @@ namespace REST
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Client_Portal v1"));
             }
 
             app.UseHttpsRedirection();
