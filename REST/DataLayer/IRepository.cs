d1d0a64e18bd521dd3aead658ea2aa842cf7a6ae
lateu <richardlateu@gmail.com>
2021-08-13 13:10:39 -0400

    added Add and GetAll methods for models

---
+++ REST/DataLayer/IRepository.cs
@@ -0,0 +1,64 @@
+﻿using REST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.DataLayer
+{
+   public interface IRepository
+    {
+        /// <summary>
+        /// add new client in the Database
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns>Clients</returns>
+        public Task<Clients> AddClient(Clients client);
+
+        /// <summary>
+        /// return list of clients
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<Clients>> GetClients();
+
+        /// <summary>
+        /// add new course in the database
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns>Courses</returns>
+        public Task<Courses> AddCourse(Courses course);
+
+        /// <summary>
+        ///  
+        /// </summary>
+        /// <returns>course list</returns>
+        public Task<List<Courses>> GetCourses();
+
+        /// <summary>
+        /// add new topic in the database
+        /// </summary>
+        /// <param name="topics"></param>
+        /// <returns>Topics object</returns>
+        public Task<Topics> AddTopic(Topics topics);
+
+        /// <summary>
+        ///   
+        /// </summary>
+        /// <returns>topics list</returns>
+       public Task<List<Topics>> GetTopics();
+
+        /// <summary>
+        /// place order for a given costumer
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="orderDetails"></param>
+        /// <returns>Orders object</returns>
+        public Task<Orders> PlaceOrder(Clients client, OrderDetails orderDetails);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>list of orders</returns>
+        public Task<List<Orders>> GetAOrders();
+    }
+}
---
+++ REST/DataLayer/RepoDB.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using REST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.DataLayer
+{
+    public class RepoDB : IRepository
+    {
+        private readonly BatchesDBContext _context;
+        public RepoDB(BatchesDBContext context) {
+            _context = context;
+        }
+        public async Task<Clients> AddClient(Clients client)
+        {
+           await _context.Clients.AddAsync(client);
+           await  _context.SaveChangesAsync();
+
+            return client;
+        }
+
+        public async Task<Courses> AddCourse(Courses course)
+        {
+            await _context.Courses.AddAsync(course);
+            await  _context.SaveChangesAsync();
+            return course;
+        }
+
+        public Task<Orders> PlaceOrder(Clients client, OrderDetails orderDetails)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async  Task<Topics> AddTopic(Topics topics)
+        {
+            await _context.Topics.AddAsync(topics);
+           await  _context.SaveChangesAsync();
+            return topics;
+        }
+
+        public async Task<List<Orders>> GetAOrders()
+        {
+            return await _context.Orders.AsNoTracking().Select(order => order).ToListAsync();
+        }
+
+        public async Task<List<Clients>> GetClients()
+        {
+            return await _context.Clients.AsNoTracking().Select(cl => cl).ToListAsync();
+        }
+
+        public async Task<List<Courses>> GetCourses()
+        {
+            return await _context.Courses.AsNoTracking().Select(c => c).ToListAsync();
+        }
+
+        public async Task<List<Topics>> GetTopics()
+        {
+            return await _context.Topics.AsNoTracking().Select(tp => tp).ToListAsync();
+        }
+    }
+}
