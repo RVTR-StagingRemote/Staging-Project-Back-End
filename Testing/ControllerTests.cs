0948c5793dde6b1ad38661fe75bdde6a16127067
Darren Yakes <dayakes@gmail.com>
2021-08-16 16:12:22 -0400

    setup unit test

--- Testing/ControllerTests.cs
+++ Testing/ControllerTests.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using REST.BusinessLayer;
+using REST.Controllers;
 using REST.DataLayer;
+using REST.Models;
 using Xunit;
 
 namespace Testing
@@ -10,7 +16,17 @@ namespace Testing
         [Fact]
         public void GetReturnsCourses()
         {
-            var mockRepo = new Mock<ICourseRepo>();
+            var mockRepo = new Mock<ICourseBL>();
+            mockRepo.Setup(x => x.GetCourses()).ReturnsAsync(new List<Courses>());
+
+            var controller = new CourseController(mockRepo.Object);
+            var result = controller.Courses();
+
+            Assert.NotNull(result);
+            Assert.IsType<Task<IActionResult>>(result);
+            
+            
+
         }
     }
 }
