83afec7716f486c1d6964809020d81a526d9cd40
Darren Yakes <dayakes@gmail.com>
2021-08-17 12:31:19 -0400

    added test for FindCourseByName and CreateCourse

--- Testing/ControllerTests.cs
+++ Testing/ControllerTests.cs
@@ -18,6 +18,7 @@ namespace Testing
         {
             mockRepo = new Mock<ICourseBL>();
         }
+
         [Fact]
         public void GetCoursesReturnsOkObjectResult()
         {
@@ -29,10 +30,11 @@ namespace Testing
 
             Assert.IsType<OkObjectResult>(result);
         }
+
         [Fact]
         public void FindCourseByIdReturnsOk()
         {
-            mockRepo.Setup(x => x.FindCourseById(1)).ReturnsAsync(new Courses(){CourseId = 1,Description = "Test Course",CourseName = "Test"});
+            mockRepo.Setup(x => x.FindCourseById(It.IsAny<int>())).ReturnsAsync(new Courses(){CourseId = 1,Description = "Test Course",CourseName = "Test"});
             var controller = new CourseController(mockRepo.Object);
 
             var response = controller.FindCourseById(1);
@@ -40,5 +42,31 @@ namespace Testing
 
             Assert.IsType<OkObjectResult>(result);
         }
+
+        [Fact]
+        public void FindCourseByNameReturnsOk()
+        {
+            mockRepo.Setup(x => x.FindCourseByName("Test")).ReturnsAsync(new Courses(){CourseId = 1,Description = "Test Course",CourseName = "Test"});
+            var controller = new CourseController(mockRepo.Object);
+
+            var response = controller.FindCourseByName("Test");
+            var result = response.Result;
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void AddCourseReturnsCreated()
+        {
+            Courses course = new Courses(){CourseId = 1,CourseName = "Test", Description = "Test Course"};
+            mockRepo.Setup(x => x.AddCourse(It.IsAny<Courses>()));
+            var controller = new CourseController(mockRepo.Object);
+
+            var response = controller.CreateCourse(course);
+            var result = response.Result;
+
+            Assert.IsType<CreatedResult>(result);
+        }
+
     }
 }
