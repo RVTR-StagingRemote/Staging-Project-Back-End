f847b4db9de860c03cb834a65c53702368588d80
Darren Yakes <dayakes@gmail.com>
2021-08-23 12:25:46 -0400

    set CalculateOrderAmount to multiply the LengthofArray*2000 being usd

--- REST/Controllers/PaymentGatewayController.cs
+++ REST/Controllers/PaymentGatewayController.cs
@@ -31,8 +31,7 @@ namespace Rest.Controllers
       var paymentIntents = new PaymentIntentService();
       var paymentIntent = paymentIntents.Create(new PaymentIntentCreateOptions
       {
-        // Amount = CalculateOrderAmount(request.CoursesToBuy),
-        Amount = request.Price, // TODO: Temporary solution, please remove when a pricing system is implemented
+        Amount = CalculateOrderAmount(request.CoursesToBuy),
         Currency = "usd",
         PaymentMethodTypes = new List<string> {
           "card",
@@ -62,7 +61,8 @@ namespace Rest.Controllers
       // Replace this constant with a calculation of the order's amount
       // Calculate the order total on the server to prevent
       // people from directly manipulating the amount on the client
-      return 20000;
+      // Placeholder amount per course that can be changed
+      return 2000*coursesToBuy.Length;
     }
   }
 }
