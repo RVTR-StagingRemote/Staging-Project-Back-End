8208afc75b8664580976305ed4a24f4f885a0a23
Chris Mesidor <christopher.mesidor1996@gmail.com>
2021-12-08 15:50:16 -0500

    Completed repo files and set up controller for applications

--- REST/BusinessLayer/INeedRepo.cs
+++ REST/BusinessLayer/INeedRepo.cs
@@ -13,13 +13,13 @@ namespace REST.BusinessLayer
         /// </summary>
         /// <param name="need"></param>
         /// <returns>Need object</returns>
-        Task<Need> AddNeed(Need need);
+        public Task<Need> AddNeed(Need need);
 
         /// <summary>
         /// get all the needs in the database
         /// </summary>
         /// <returns>List of needs</returns>
-        Task<List<Need>> GetNeeds();
+        public Task<List<Need>> GetNeeds();
 
         /// <summary>
         /// get a need by need id
@@ -27,20 +27,20 @@ namespace REST.BusinessLayer
         /// <param name="id"></param>
         /// <returns>Need object</returns>ram>
         /// <returns></returns>
-        Task<Need> GetNeedsById(int Id);
+        public Task<Need> GetNeedsById(int Id);
 
         /// <summary>
         /// updates a need's info in the database
         /// </summary>
         /// <param name="need"></param>
         /// <returns>Needs object or null if no object found</returns>
-        Task<Need> UpdateNeeds(Need need);
+        public Task<Need> UpdateNeeds(Need need);
 
         /// <summary>
         /// remove a need from the data base by need id
         /// </summary>
         /// <param name="needtId"></param>
         /// <returns>Needs object</returns>
-        Task<Need> DeleteNeedById(int needId);
+        public Task<Need> DeleteNeedById(int needId);
     }
 }
